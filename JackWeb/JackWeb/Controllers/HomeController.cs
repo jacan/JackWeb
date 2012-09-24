497d87dd06037e04f2085b6256b7a1f185f57b98
jacan <jckandersen@gmail.com>
2012-09-24 12:55:21 +0200

    Tabs

--- JackWeb/JackWeb/Controllers/HomeController.cs
+++ JackWeb/JackWeb/Controllers/HomeController.cs
@@ -6,27 +6,27 @@ using System.Web.Mvc;
 
 namespace JackWeb.Controllers
 {
-    public class HomeController : Controller
-    {
-        public ActionResult Index()
-        {
-            ViewBag.Message = "Let's build some new stuff..";
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			ViewBag.Message = "Let's build some new stuff..";
 
-            return View();
-        }
+			return View();
+		}
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "A playground/sandbox for a software developer in constant development";
+		public ActionResult About()
+		{
+			ViewBag.Message = "A playground/sandbox for a software developer in constant development";
 
-            return View();
-        }
+			return View();
+		}
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Should you have any interest in contacting me personally, please checkout my details";
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Should you have any interest in contacting me personally, please checkout my details";
 
-            return View();
-        }
-    }
+			return View();
+		}
+	}
 }

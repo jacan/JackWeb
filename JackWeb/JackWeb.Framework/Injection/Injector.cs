2ed8723eaa280aeebfd67ff485ef5edf3b264e2b
= <=>
2012-09-24 14:22:14 +0200

    Tabs

--- JackWeb/JackWeb.Framework/Injection/Injector.cs
+++ JackWeb/JackWeb.Framework/Injection/Injector.cs
@@ -9,41 +9,42 @@ using JackWeb.Data;
 
 namespace JackWeb.Framework.Injection
 {
-    public class Injector : IInject
-    {
-        public void Prepare()
-        {
-            ObjectFactory.Initialize(x =>
-            {
-                x.For<ISessionFactory>()
-                    .Singleton()
-                    .Use(() => new NSessionConfiguration("Azure").CreateSession());
+	public class Injector : IInject
+	{
+		public void Prepare()
+		{
+			ObjectFactory.Initialize(x =>
+			{
+				x.For<ISessionFactory>()
+					.Singleton()
+					.Use(() => new NSessionConfiguration("Azure").CreateSession());
 
-                x.For<ISession>()
-                    .HybridHttpOrThreadLocalScoped()
-                    .Use(() => ObjectFactory.GetInstance<ISessionFactory>().OpenSession());
+				x.For<ISession>()
+					.HybridHttpOrThreadLocalScoped()
+					.Use(() => ObjectFactory.GetInstance<ISessionFactory>().OpenSession());
 
-                x.For<IInject>()
-                    .Use<Injector>();
+				x.For<IInject>()
+					.Use<Injector>();
 
-                x.Scan(scan =>  {
-                    scan.Assembly(typeof(IRepository<>).Assembly);
-                    scan.WithDefaultConventions();
+				x.Scan(scan =>
+				{
+					scan.Assembly(typeof(IRepository<>).Assembly);
+					scan.WithDefaultConventions();
 
-                    x.For(typeof(IRepository<>)).Use(typeof(Repository<>));
+					x.For(typeof(IRepository<>)).Use(typeof(Repository<>));
 
-                });
-            });
-        }
+				});
+			});
+		}
 
-        public T GetInstance<T>()
-        {
-            return ObjectFactory.GetInstance<T>();
-        }
+		public T GetInstance<T>()
+		{
+			return ObjectFactory.GetInstance<T>();
+		}
 
-        public object GetInstance(Type t)
-        {
-            return ObjectFactory.GetInstance(t);
-        }
-    }
+		public object GetInstance(Type t)
+		{
+			return ObjectFactory.GetInstance(t);
+		}
+	}
 }
--- JackWeb/JackWeb.Tests/TestBase.cs
+++ JackWeb/JackWeb.Tests/TestBase.cs
@@ -1,29 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using JackWeb.Framework.Injection;
-using JackWeb.Framework;
+﻿using JackWeb.Framework.Configuration.Injection;
 using NHibernate;
 
 namespace JackWeb.Tests
 {
-    public class TestBase
-    {
-        private IInject _injector;
+	public class TestBase
+	{
+		private IInject _injector;
 
-        public TestBase()
-        {
-            _injector = new Injector();
-            _injector.Prepare();
-        }
+		public TestBase()
+		{
+			_injector = new Injector();
+			_injector.Prepare();
+		}
 
-        protected ISession Session
-        {
-            get
-            {
-                return _injector.GetInstance<ISession>();
-            }
-        }
-    }
+		protected ISession Session
+		{
+			get
+			{
+				return _injector.GetInstance<ISession>();
+			}
+		}
+	}
 }

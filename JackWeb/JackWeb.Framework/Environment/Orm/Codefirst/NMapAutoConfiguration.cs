53bb381f0486080e7c19ded7ca621b7ddca17ac2
jacan <jckandersen@gmail.com>
2012-09-24 12:52:36 +0200

    Added, some Codefirst automapping plumbing

---
+++ JackWeb/JackWeb.Framework/Environment/Orm/Codefirst/NMapAutoConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentNHibernate.Automapping;
+using JackWeb.Framework.Extensions;
+
+namespace JackWeb.Framework.Environment.Orm.Codefirst
+{
+	public class NMapAutoConfiguration : DefaultAutomappingConfiguration
+	{
+		protected IList<string> _namespaceToMap;
+
+		public NMapAutoConfiguration(params string[] namespacesToMap)
+		{
+			_namespaceToMap = namespacesToMap.ToList();
+		}
+
+		public override bool ShouldMap(Type type)
+		{
+			return _namespaceToMap
+				.SingleOrDefault(x => x.Equals(_namespaceToMap))
+				.IsNotNull();
+		}
+	}
+}

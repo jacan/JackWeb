3d396d671d249cdeea616599cab2085136f7275c
jacan <jckandersen@gmail.com>
2012-09-24 12:51:57 +0200

    Added NullExtensions

---
+++ JackWeb/JackWeb.Framework/Extensions/NullExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JackWeb.Framework.Extensions
+{
+	public static class NullExtensions
+	{
+		public static bool IsNull(this object theObject)
+		{
+			return theObject == null;
+		}
+
+		public static bool IsNotNull(this object theObject)
+		{
+			return theObject != null;
+		}
+	}
+}

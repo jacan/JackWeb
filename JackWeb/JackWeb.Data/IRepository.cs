fec7a28cff1891bf806f1d8c280973a1cf36f5cb
jacan <jckandersen@gmail.com>
2012-10-02 11:19:13 +0200

    Added DoStuff to IRepository

--- JackWeb/JackWeb.Data/IRepository.cs
+++ JackWeb/JackWeb.Data/IRepository.cs
@@ -8,8 +8,10 @@ namespace JackWeb.Data
     public interface IRepository<TEntity>
         where TEntity : class
     {
+		void DoStuff();
         void Add(TEntity entity);
         IEnumerable<TEntity> GetAll();
         IEnumerable<TEntity> GetAll(Func<TEntity, bool> predicate, bool has);
+		// some crappy change
     }
 }
